347ca4b3579a6fb8dbd1cc1084a1ff7a6c119e95
sandu345 <sandunibatugedara11@gmail.com>
2024-05-28 14:18:12 +0530

    backend

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -7,13 +7,27 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions/v2/https");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+exports.generateText  = functions.https.onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+
+    if (request.method !== "POST") {
+        return response
+            .status(400)
+            .setDefaultEncoding({message: "only post method is supported."});
+    }
+
+    const { prompt, id } = request.body;
+
+    if (!prompt) {
+        return response.status(400).send({message: "'prompt' arg is required" })
+    }
+     response.send("Hello from Firebase!");
 });
